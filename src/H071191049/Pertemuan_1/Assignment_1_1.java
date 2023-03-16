405720a6b2e95039b0b34e59a7587f7857b58753
toku404 <phantom26isn@gmail.com>
2023-03-16 17:59:12 +0800

    fix error

--- src/H071191049/Pertemuan_1/Assignment_1_1.java
+++ src/H071191049/Pertemuan_1/Assignment_1_1.java
@@ -1,7 +1,7 @@
 package H071191049.Pertemuan_1;
 
 public class Assignment_1_1 {
-	public stati void main(String[] args){
+	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
 }
